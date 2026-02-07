8936445efb0ccc055870b2201ca4d666456afe2b
Mayank Tilwankar <mayanktilwankar2355@gmail.com>
2026-02-07 15:07:04 +0530

    ui updates

--- pages/index.js
+++ pages/index.js
@@ -16,16 +16,6 @@ const Index = () => {
         <title>
           Best Car Rental in india | Most Affordable Car Hire Services india
         </title>
-        <meta name="description" content="" />
-        <meta name="keywords" content="" />
-        <meta
-          property="og:title"
-          content="Best Car Rental in india | Top-Rated Vehicle Hire Services"
-        />
-        <meta
-          property="og:description"
-          content="Leading car rental service in india offering wide range of vehicles. Best prices guaranteed for self-drive cars and chauffeur services across india."
-        />
       </Head>
       <Loader />
       <Navbar />
